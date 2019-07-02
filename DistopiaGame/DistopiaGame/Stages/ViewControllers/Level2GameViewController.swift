9cc60e412c11efd4d22a7c8a99ad67305d5cd44f
Luma Gabino <lumagabino@gmail.com>
2019-07-02 14:54:59 -0300

    level 2 - camera mechanics

---
+++ DistopiaGame/DistopiaGame/Stages/ViewControllers/Level2GameViewController.swift
@@ -0,0 +1,50 @@
+//
+//  CameraGameViewController.swift
+//  DistopiaGame
+//
+//  Created by Luma Gabino Vasconcelos on 02/07/19.
+//  Copyright © 2019 anthony.gianeli. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class Level2GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "Level2GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
