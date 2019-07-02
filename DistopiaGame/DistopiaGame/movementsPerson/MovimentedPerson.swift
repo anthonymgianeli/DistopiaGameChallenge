11baeeb0d1cd7cb20173742f1b82e3be25f37857
Anthony Marques Gianeli <antony.marques@mblabs.com.br>
2019-07-02 12:07:29 -0300

    Estrutura

---
+++ DistopiaGame/DistopiaGame/movementsPerson/MovimentedPerson.swift
@@ -0,0 +1,9 @@
+//
+//  MovimentedPerson.swift
+//  DistopiaGame
+//
+//  Created by anthony gianeli on 02/07/19.
+//  Copyright © 2019 anthony.gianeli. All rights reserved.
+//
+
+import Foundation
