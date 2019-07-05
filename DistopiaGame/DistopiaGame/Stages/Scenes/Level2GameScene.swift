774b1847a1969eff448895f036ff029acfa93c89
Luma Gabino <lumagabino@gmail.com>
2019-07-05 11:33:42 -0300

    reconhecimento de contacto/colisao e inicio do jump

--- DistopiaGame/DistopiaGame/Stages/Scenes/Level2GameScene.swift
+++ DistopiaGame/DistopiaGame/Stages/Scenes/Level2GameScene.swift
@@ -3,20 +3,43 @@
 import SpriteKit
 import GameplayKit
 
-class Level2GameScene: SKScene {
-    
-    //    private var lightNode = SKLightNode()
-    //    private var background = SKSpriteNode(imageNamed: "fundo")
+struct ColliderType {
+    static let None: UInt32 = 0
+    static let Character: UInt32 = 1
+    static let Camera: UInt32 = 2
+    static let Ground: UInt32 = 4
+}
+
+class Level2GameScene: SKScene, SKPhysicsContactDelegate {
     var cameraImage: SKSpriteNode?
+    var cameraBody: SKSpriteNode?
     var lightNode: SKLightNode?
     var lamp: SKSpriteNode?
     var character: SKSpriteNode?
+    var ground: SKSpriteNode?
+    
+    var characterOnGround:Bool = true
+    
+    var movingTouch: CGPoint!
+    var firstTouch: CGPoint!
+    var movingCenter: CGPoint!
+    var movimentConstant: CGFloat = 20
+    let screenSize: CGRect = UIScreen.main.bounds
+    
+    var count:Int = 0
     
     
     
     override func didMove(to view: SKView) {
         self.character = self.childNode(withName: "character2") as? SKSpriteNode
         self.cameraImage = childNode(withName: "Camera") as? SKSpriteNode
+        self.ground = childNode(withName: "ground2") as? SKSpriteNode
+        self.cameraBody = cameraImage!.childNode(withName: "CameraBody") as? SKSpriteNode
+        
+//        let center = SKSpriteNode()
+//        center.position = CGPoint(x: -358.468, y: 204.451)
+//        cameraImage!.addChild(center)
+
         
         let rotateAnti = SKAction.rotate(byAngle: .pi/6, duration: 0.7)
         let rotateClockwise = SKAction.rotate(byAngle: -(.pi/6), duration: 0.7)
@@ -25,36 +48,104 @@ class Level2GameScene: SKScene {
         cameraImage!.run(repeatAction)
         
         
-        //      Ponto de luz
-        //        background.size = CGSize(width: self.size.height, height: self.size.height)
-        ////        background.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-        ////        background.position = CGPoint(x: self.size.width/2, y:self.size.height/2)
-        ////        background.zPosition = -1
-        //
-        //
-        //        background.lightingBitMask = 1
-        //        background.shadowCastBitMask = 0
-        //        background.shadowedBitMask = 1
-        //        addChild(background)
-        //
-        
-        //self.lightNode = childNode(withName: "Light") as? SKLightNode
-        //        lightNode!.categoryBitMask = 1
-        //        lightNode!.falloff = 0
-        //        lightNode!.ambientColor = UIColor.white
-        //        lightNode!.lightColor = UIColor(red: 1.0, green: 1.0, blue: 0.0, alpha: 0.5)
-        //        lightNode!.shadowColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.3)
-        //        lightNode!.setScale(50)
-        
+        physicsWorld.contactDelegate = self
+        character!.physicsBody?.categoryBitMask = ColliderType.Character
+        cameraBody!.physicsBody?.categoryBitMask = ColliderType.Camera
+        character!.physicsBody?.collisionBitMask = ColliderType.Ground
+        //Operadores binários: AND(&), OR(|)
+        //A soma dos valores sem carry tambem e valida para OR
+        character!.physicsBody?.contactTestBitMask = ColliderType.Camera
+        cameraBody!.physicsBody?.contactTestBitMask = ColliderType.Character
+    
         
     }
     
-    //Testar se funciona
-    //Inverter if - não sei qual é o A  e o B (?)
     func didBegin(_ contact: SKPhysicsContact) {
-        if contact.bodyA == self.cameraImage && contact.bodyB == self.character {
-            print("Encostou na camera!")
+        if (contact.bodyA == self.cameraBody?.physicsBody && contact.bodyB == self.character?.physicsBody) ||
+            (contact.bodyA == self.character?.physicsBody && contact.bodyB == self.cameraBody?.physicsBody) {
+            count+=1
+            print("Encostou na camera!  \(count)")
+        }
+        
+        if (contact.bodyA == self.character?.physicsBody && contact.bodyB == self.ground?.physicsBody) ||
+            (contact.bodyA == self.ground?.physicsBody && contact.bodyB == self.character?.physicsBody) {
+            characterOnGround = true
+            print("true")
+        }
+        
+    }
+    
+    
+    func didEnd(_ contact: SKPhysicsContact) {
+        if (contact.bodyA == self.cameraBody?.physicsBody && contact.bodyB == self.character?.physicsBody) ||
+            (contact.bodyA == self.character?.physicsBody && contact.bodyB == self.cameraBody?.physicsBody){
+            print("Ta fora!")
+        }
+        
+        if (contact.bodyA == self.character?.physicsBody && contact.bodyB == self.ground?.physicsBody) ||
+            (contact.bodyA == self.ground?.physicsBody && contact.bodyB == self.character?.physicsBody) {
+            characterOnGround = false
+            print("false")
+        }
+    }
+        
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            firstTouch = touch.location(in: self)
+            movingCenter = touch.location(in: self)
         }
     }
     
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            movingTouch = touch.location(in: self)
+
+//            if Double(movingTouch.x) > Double(movingCenter.x) {
+//                moveRight()
+//                movingCenter.x = movingTouch.x - 0.1
+//            } else if Double(movingTouch.x) < Double(movingCenter.x) {
+//                moveLeft()
+//                movingCenter.x = movingTouch.x + 0.1
+//            } else if Double(movingTouch.y) != Double(movingCenter.y)
+            if characterOnGround {
+                jump()
+
+            }
+        
+            
+        }
+        
+    }
+    
+    func moveRight() {
+        let maxFingerMoviment = screenSize.width/4
+        let fingerDistance = movingTouch.x - movingCenter.x
+        let velocityConstant = fingerDistance/maxFingerMoviment
+        
+        let moveRight = SKAction.moveBy(x: movimentConstant * velocityConstant, y: 0, duration: 0.5)
+        character?.run(moveRight)
+    }
+    
+    func moveLeft() {
+        let moveLeft = SKAction.moveBy(x: -movimentConstant, y: 0, duration: 1)
+        character?.run(moveLeft)
+        
+    }
+    
+    @objc func jump() {
+        character!.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 5))
+        
+//        let minX: CGFloat = 200 // some suitable value
+//        let multiplier: CGFloat = 5
+//        let force = max(0, (character!.position.x / minX - 1) * multiplier)
+//        character!.physicsBody?.applyImpulse(CGVector(dx: -force, dy: 0))
+        
+//        let jump = SKAction.moveBy(x: 0, y: 50, duration: 1)
+//        let moveRight = SKAction.moveBy(x: 50 , y: 0, duration: 0.5)
+//        let group = SKAction.group([jump, moveRight])
+//        character?.run(group)
+    }
+    
 }
+
